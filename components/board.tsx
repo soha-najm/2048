8e4a388f586382c082d73a94840b308b789164d3
soha <sohanajmeddin@gmail.com>
2024-04-11 12:13:21 +0330

    boaed styles and some configs

---
+++ components/board.tsx
@@ -0,0 +1,21 @@
+import styles from "@/styles/board.module.css";
+
+function Board() {
+  const renderGrid = () => {
+    const cells: JSX.Element[] = [];
+    const totalCellCounts = 16;
+
+    for (let index = 0; index < totalCellCounts; index++) {
+      cells.push(<div key={index} className={styles.cell}></div>);
+    }
+    return cells;
+  };
+  return (
+    <div className={`${styles.board}`}>
+      <div className={styles.grid}>{renderGrid()}</div>
+    </div>
+  );
+}
+
+export default Board;
+ 
